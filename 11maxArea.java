0729bc2aef7099fc541dc5032666301c24a53f45
binbinche <1095988619@qq.com>
2018-06-03 20:35:36 +0800

    first commit

---
+++ 11maxArea.java
@@ -0,0 +1,18 @@
+/*给定 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。画 n 条垂直线，使得垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+
+注意：你不能倾斜容器，n 至少是2。*/
+class Solution {
+    public int maxArea(int[] height) {
+        int maxarea = 0;
+        int left = 0, right=height.length-1;
+        while(left<right){
+            maxarea = Math.max(maxarea, Math.min(height[left],height[right])*(right-left));
+            if(height[left]<height[right])
+                left++;
+            else
+                right--;
+                
+        }
+        return maxarea;
+    }
+}
