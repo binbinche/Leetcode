e2a76b278e05d0bb1340429d72aeef82e867622d
binbinche <1095988619@qq.com>
2018-12-26 20:25:22 +0800

    19.删除链表的倒数第N个节点

---
+++ removeNthFromEnd.java
@@ -0,0 +1,33 @@
+public class removeNthFromEnd {
+    /**给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+     示例：
+     给定一个链表: 1->2->3->4->5, 和 n = 2.
+     当删除了倒数第二个节点后，链表变为 1->2->3->5.
+     说明：
+     给定的 n 保证是有效的。
+     进阶：
+     你能尝试使用一趟扫描实现吗？
+     * */
+    public class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int x){val = x;}
+    }
+    public ListNode solutionRemoveNthFromEnd(ListNode head, int n){
+        ListNode p = head;
+        ListNode q = head;
+        while(n!=0){
+            p = p.next;
+            n--;
+        }
+        if(p==null)
+            return p.next;
+        while(p.next !=null){
+            p = p.next;
+            q = q.next;
+
+        }
+        q.next = q.next.next;
+        return head;
+    }
+}
